9ccd9ed988119eec0234975683fab8edb6ce54e7
parrots38 <80542158+parrots38@users.noreply.github.com>
2021-03-21 22:58:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 
 
-TOKEN = 'a06cdcad832d15e530f4f0a5f539424dabb51605a8766762b5d4c5ea07f' \
-        '9fc63d5b966dc5b8c6dd635f62'
+TOKEN = ''
 
-group_token = 'cfc0f60e93c0198e91414180951d212388bcafd3eda79e9d197f1fb09574f5654b95d4687e9246ac74b20'
-group_id = '197499067'
+group_token = ''
+group_id = ''
